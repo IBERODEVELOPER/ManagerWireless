acb8f8a9bf01b007447d8bb43a73a5641da29867
El Informatico Ibero <tamani1995@gmail.com>
2022-12-11 10:38:09 -0500

    Actualizando Raul Tamani

--- app/src/main/java/informatico/to/managerwireless/MainActivity.java
+++ app/src/main/java/informatico/to/managerwireless/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -24,28 +25,34 @@ public class MainActivity extends AppCompatActivity {
         /*Enlazamos el objeto de la clase imagenview con el id del xml*/
         imagen= (ImageView) findViewById(R.id.imgVwireless);
         /*Capturamos el estado del servicio*/
-        admini_wifi=(WifiManager)this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        admini_wifi=(WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         /*consultamos si el servicio esta activo entonces que me muestre segun el
          metodo setimagewifi la imagen correspondientes*/
         setimagenwifi(admini_wifi.isWifiEnabled());
-
+        /*if (admini_wifi.isWifiEnabled()==false){
+            System.out.println("Encendiendo...");
+            admini_wifi.setWifiEnabled(true);
+        }else{
+            System.out.println("Apagando...");
+            admini_wifi.setWifiEnabled(false);
+        }*/
     }
-/*Metodo para el onclick*/
+    //Metodo para el onclick
     public void click_imagen(View v){
         setestadowifi();
     }
-    /*creamos un metodo que me devuelva un valor booleano */
+    //creamos un metodo que me devuelva un valor booleano
     public void setimagenwifi(boolean valor){
-        /*si el valor es verdadero entonces me mostrar la imagen wifi_on*/
+        //si el valor es verdadero entonces me mostrar la imagen wifi_on
         if (valor)imagen.setImageResource(R.drawable.wifi_on);
-        /*De lo contrario la imagen wifi_off*/
+        //De lo contrario la imagen wifi_off
         else imagen.setImageResource(R.drawable.wifi_off);
 
     }
-    /*Metodo para saber el estado del servicio wifi*/
+    //Metodo para saber el estado del servicio wifi
     public void setestadowifi()
     {
-        /*consultamos si el wifi es diferente de enable entonces me mostrara la imagen correspondiente*/
+        //consultamos si el wifi es diferente de enable entonces me mostrara la imagen correspondiente
         setimagenwifi(!admini_wifi.isWifiEnabled());
         admini_wifi.setWifiEnabled(!admini_wifi.isWifiEnabled());
     }
